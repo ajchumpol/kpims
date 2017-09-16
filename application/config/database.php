60ea6f3ab3dd3282749599c9474b7509244a1190
root <chumpol.mok@cpc.ac.th>
2017-09-16 20:25:00 +0700

    KPI - files name update.

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '127.0.0.1',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'KPIFund_DB',
+	'hostname' => 'localhost',
+	'username' => 'kpiadmin',
+	'password' => 'AdminPass',
+	'database' => 'kpifund_db',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
